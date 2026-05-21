5d3546a7e7258ca3c07cd7550e379ddf5b71cdef
Tejesh_Bhagat <168338113+Tejesh-dev@users.noreply.github.com>
2026-05-21 09:41:27 +0000

    file added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+//varivales are usage to tore data or valu in a contenaer
+
+// there are three types of variables 
+
+
+let name = "Tejesh"
+var age = 24
+const ID = "tejbhagat2001@gmail.com"
+
+console.log([name, age, ID]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+// datatypes are usage to store different types of data in a varibale
+
+
+const name = "Tejesh"
+
+const age = 24
+
+const isStudent = "true"
+
+cosnt id = 1.344344
+
+console.log.table
