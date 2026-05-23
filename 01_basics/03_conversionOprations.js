bcfd1331942a6cb57da3ab539a8c57eaa6a2af62
Tejesh_Bhagat <168338113+Tejesh-dev@users.noreply.github.com>
2026-05-23 09:26:18 +0000

    LEARN CONVERTIONS

---
+++ 01_basics/03_conversionOprations.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valuInNumber = Number(score)
+
+console.log(typeof valuInNumber)
+console.log(valuInNumber)
+
+// "33" => 33
+//"33abc" => Nan
+// true => 1
