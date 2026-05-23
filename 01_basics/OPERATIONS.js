a1cc6130dac25c90e8514d07b48891cf0b10cb53
Tejesh_Bhagat <168338113+Tejesh-dev@users.noreply.github.com>
2026-05-23 10:06:11 +0000

    C

---
+++ 01_basics/OPERATIONS.js
@@ -0,0 +1,25 @@
+let value = 3
+
+let negValue = -value
+
+// console.log(negValue);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+
+console.log("1" + 2); 
+// "12" → string + number = number converts into string → concatenation
+
+console.log(1 + "2"); 
+// "12" → number + string = number converts into string → concatenation
+
+console.log("1" + 2 + 2); 
+// "122" → left to right: "1"+2 = "12", then "12"+2 = "122"
+
+console.log(1 + 2 + "2"); 
+// "32" → left to right: 1+2 = 3, then 3+"2" = "32"
+
+
+
